e533dc4ee1184d96f826f0300927d275a58d0a10
MERT SAVAŞ <50775111+dev-Mjollnir@users.noreply.github.com>
2021-06-19 14:09:00 +0300

    Add files via upload

---
+++ MarsRoverUnitTest/Provider/Position.cs
@@ -0,0 +1,114 @@
+﻿using MarsRoverProblem.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace MarsRoverProblem
+{
+
+    public interface IPosition
+    {
+        void StartMoving(List<int> maxPoints, string moves);
+    }
+
+    public class Position : IPosition
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Directions Direction { get; set; }
+
+        public Position()
+        {
+            X = Y = 0;
+            Direction = Directions.N;
+        }
+
+        private void Rotate90Left()
+        {
+            switch (this.Direction)
+            {
+                case Directions.N:
+                    this.Direction = Directions.W;
+                    break;
+                case Directions.S:
+                    this.Direction = Directions.E;
+                    break;
+                case Directions.E:
+                    this.Direction = Directions.N;
+                    break;
+                case Directions.W:
+                    this.Direction = Directions.S;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Rotate90Right()
+        {
+            switch (this.Direction)
+            {
+                case Directions.N:
+                    this.Direction = Directions.E;
+                    break;
+                case Directions.S:
+                    this.Direction = Directions.W;
+                    break;
+                case Directions.E:
+                    this.Direction = Directions.S;
+                    break;
+                case Directions.W:
+                    this.Direction = Directions.N;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MoveInSameDirection()
+        {
+            switch (this.Direction)
+            {
+                case Directions.N:
+                    this.Y += 1;
+                    break;
+                case Directions.S:
+                    this.Y -= 1;
+                    break;
+                case Directions.E:
+                    this.X += 1;
+                    break;
+                case Directions.W:
+                    this.X -= 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void StartMoving(List<int> maxPoints, string moves)
+        {
+            foreach (var move in moves)
+            {
+                switch (move)
+                {
+                    case 'M':
+                        this.MoveInSameDirection();
+                        break;
+                    case 'L':
+                        this.Rotate90Left();
+                        break;
+                    case 'R':
+                        this.Rotate90Right();
+                        break;
+                    default:
+                        throw new Exception($"Invalid Character {move}");
+                }
+
+                if (this.X < 0 || this.X > maxPoints[0] || this.Y < 0 || this.Y > maxPoints[1])
+                {
+                    throw new Exception($"Position can not be beyond bounderies (0 , 0) and ({maxPoints[0]} , {maxPoints[1]})");
+                }
+            }
+        }
+    }
+}
